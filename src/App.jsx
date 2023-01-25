4865f560e87f4b0ac6414dd413a415436624e31f
Joakim Villo <villokodehode@gmail.com>
2023-01-25 19:12:39 +0100

    almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,36 @@ import PRODUCT from "./assets/image-product-desktop.jpg";
 
 function App() {
   return (
-    <div className="flex h-96 w-[40rem] rounded-xl flex-row bg-base-100 shadow-xl">
+    <div className="flex h-[35rem] w-[40rem] rounded-xl flex-row bg-base-100 shadow-xl font-montserrat">
       <figure className="">
         <img
           className="h-full w-[20rem] rounded-l-xl"
           src={PRODUCT}
-          alt="Shoes"
+          alt="Gabrielle CHANEL"
         />
       </figure>
-      <div className="w-[20rem] text-black">
-        <h2 className="text-xl text-left p-4 tracking-widest pl-12 text-green-800">
+      <div className="w-[20rem] pl-8 text-design-darkblue">
+        <h2 className="text-xl pt-8 text-left tracking-widest text-design-darkblue">
           PERFUME
         </h2>
-        <p className="text-black text-[2.6rem] leading-10 text-left pl-12">
+        <p className="text-design-darkblue font-fraunces text-[2.6rem] leading-10 text-left py-4">
           Gabrielle Essence Eau De Parfum
         </p>
-        <p>
+        <p className="text-start">
           A floral, solar and voluptuous interpretation composed by Oliver
           Polge, Perfumer-Creator for the House of CHANEL.
         </p>
-        <div>
-          <p className="text-green-800">
-            $149.99 <span className="px-8">$169.99</span>
+        <div className="relative text-start mx-8">
+          <p className="text-design-darkcyan font-fraunces text-3xl py-8">
+            $149.99{" "}
+            <span className="mx-5 absolute top-1/2 -translate-y-1/2 text-base line-through font-montserrat text-design-darkgrayblue">
+              $169.99
+            </span>
           </p>
         </div>
-        <div className="flex flex-col w-fit justify-center content-end m-auto h-full">
+        {/* <div className="flex flex-col w-fit justify-center content-end m-auto h-full">
           <button className="bg-black">Buy Now</button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fraunces: ["Fraunces", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        design: {
+          darkcyan: "hsl(158, 36%, 37%)",
+          cream: "hsl(30, 38%, 92%)",
+          darkblue: "hsl(212, 21%, 14%)",
+          darkgrayblue: "hsl(228, 12%, 48%)",
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
